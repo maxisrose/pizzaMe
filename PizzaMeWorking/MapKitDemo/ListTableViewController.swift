ec8850aad5924ac85b67d2694f3d900ba2069be0
Maxim Rose <maxisrose@Maxims-MacBook-Pro.local>
2016-07-28 21:52:06 -0700

    added listView controller and added numrows and cell for row functions with dummy text

--- PizzaMeWorking/MapKitDemo/ListTableViewController.swift
+++ PizzaMeWorking/MapKitDemo/ListTableViewController.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2016 Matthew Porter. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+import CoreLocation
+
+class ListTableViewController: UITableViewController {
+    
+    var itemsArray: [MKMapItem] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("pizzaCell")!
+        cell.textLabel?.text = "Pizza Butt"
+        cell.detailTextLabel?.text = "1-(800)-CALLPIZZABUTT"
+        return cell
+    }
+}
